cc697168f8c44930c647d9b4fc1013bbe8d13b6d
Max Luzuriaga <max@luzuriaga.com>
2013-09-16 17:53:17 -0400

    Added varing fittext contents for different parts of the screen.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
-	$("body").fitText(3.0);
+	$("header").fitText(3.0);
+	$("#schedule li").fitText(4.0);
+	$("#schedule li.current-class .block").fitText(1.0);
 	$("footer").fitText(0.2);
 });

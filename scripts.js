9aa8ca84db6a8731c1f5c54285c5308d2d569aea
Max Luzuriaga <max@luzuriaga.com>
2013-09-16 18:32:36 -0400

    Formatted temperature information with CSS

--- scripts.js
+++ scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	$("header").fitText(3.0);
 	$("#schedule li").fitText(4.0);
 	$("#schedule li.current-class .block").fitText(1.0);
-	$("footer").fitText(0.2);
+	$("footer #current-temp").fitText(0.12);
+	$("footer #temps").fitText(0.2);
 });

2960c89d328e7768ab09c0042a560093e142e11b
Max Luzuriaga <max@luzuriaga.com>
2013-09-16 21:05:42 -0400

    Automatic updating of date and time every second.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,42 @@
+function updatePage() {
+	// Update date and time
+	var m_names = new Array("January", "February", "March", 
+"April", "May", "June", "July", "August", "September", 
+"October", "November", "December");
+	var d_names = new Array("Sunday", "Monday", "Tuesday",
+"Wednesday", "Thursday", "Friday", "Saturday");
+
+	var d = new Date();
+
+	var curr_day = d.getDay();
+	var curr_date = d.getDate();
+	var curr_month = d.getMonth();
+
+	var curr_hour = d.getHours() % 12;
+	var curr_min = d.getMinutes();
+
+	curr_min = curr_min + "";
+	
+	if (curr_min.length == 1) {
+		curr_min = "0" + curr_min;
+	}
+
+	if (curr_hour == 0) {
+		curr_hour = 12;
+	}
+
+	$("header .time").html(curr_hour + ":" + curr_min)
+	$("header .weekday").html(d_names[curr_day]);
+	$("header .date").html(m_names[curr_month] + " " + curr_date);
+}
+
+window.setInterval(function() {
+	updatePage();
+}, 1000);
+
 $(document).ready(function(){
+	updatePage();
+
 	$("header").fitText(3.0);
 	$("#schedule li").fitText(4.0);
 	$("#schedule li.current-class .block").fitText(1.0);

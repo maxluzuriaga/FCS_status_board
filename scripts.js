47be91d62085a14f3eb88fe5ffca129e78d8bbb4
Max Luzuriaga <max@luzuriaga.com>
2013-09-16 23:43:57 -0400

    Increased size of block names

--- scripts.js
+++ scripts.js
@@ -127,7 +127,8 @@ $(document).ready(function(){
 
 	$("header").fitText(3.0);
 	$("#schedule li").fitText(4.0);
-	$("#schedule li.current-class .block").fitText(1.0);
+	$("#schedule li .block").fitText(2.0);
+	$("#schedule li.current-class .block").fitText(0.7);
 	$("footer #current-temp").fitText(0.12);
 	$("footer #temps").fitText(0.2);
 });
